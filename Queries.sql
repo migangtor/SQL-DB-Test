b1905a70f8dd9f0abe6408b03b2634f3b3763835
Miguel Torres <migangtor@hotmail.com>
2024-04-27 16:09:01 +0100

    feat: add additional queries

--- Queries.sql
+++ Queries.sql
@@ -36,6 +36,58 @@ FROM
 	FROM EMPLOYEE) as rn
 WHERE rn.rank_salary <=3 
 
+-- Running total, count, average and quartile
+SELECT *,
+SUM(salary) OVER(ORDER BY emp_id) as running_tot,
+COUNT(salary) OVER(ORDER BY emp_id) as running_count,
+AVG(salary) OVER(ORDER BY emp_id) as running_avg,
+NTILE(4) OVER(ORDER BY emp_id) as quartile
+FROM EMPLOYEE;
+
+-- Lag and Lead
+
+SELECT *,
+LAG(salary) OVER (PARTITION BY dept_name) as lag,
+LAG(salary,2) OVER (PARTITION BY dept_name) as lag2,
+LAG(salary,2,0) OVER (PARTITION BY dept_name) as lag3,
+LEAD(salary) OVER (PARTITION BY dept_name) as lead
+FROM EMPLOYEE
+
+
+-- First and Last Value
+
+SELECT *,
+first_value(emp_name) over (partition by dept_name order by salary) as least_paid,
+first_value(emp_name) over (partition by dept_name order by salary desc) as highest_paid
+FROM EMPLOYEE
+
+
+
+SELECT *,
+--first_value(emp_name) over (partition by dept_name order by salary) as least_paid,
+last_value(emp_name) over (partition by dept_name order by salary desc
+						  range between unbounded preceding and current row
+						  ) as highest_paid
+FROM EMPLOYEE
+
+-- fetch a query to display if the salary of an employee is higher, lower
+-- or equal to the previous employee
+
+
+SELECT *,
+LAG(salary,1,0) OVER (PARTITION BY dept_name ORDER BY emp_id) as prev_emp_salary,
+CASE 
+	WHEN salary > LAG(salary,1,0) OVER (PARTITION BY dept_name ORDER BY emp_id) THEN 'High'
+	WHEN salary = LAG(salary,1,0) OVER (PARTITION BY dept_name ORDER BY emp_id) THEN 'Same'
+	WHEN salary < LAG(salary,1,0) OVER (PARTITION BY dept_name ORDER BY emp_id) THEN 'Low'
+END AS test
+FROM EMPLOYEE
+
+
+
+
+
+
 
 
 
