f2178ed29582f0d0d91fa28b11014d4e6eb13c28
Miguel Torres <migangtor@hotmail.com>
2024-04-30 12:14:27 +0100

    feat: add question and solution to query 3

--- Top10Queries.sql
+++ Top10Queries.sql
@@ -81,7 +81,40 @@ from employee;
 
 
 --- **************************************************************************************** ---               
+--- Q3 : Find actual distance --- 
 
+drop table if exists car_travels;
+create table car_travels
+(
+    cars                    varchar(40),
+    days                    varchar(10),
+    cumulative_distance     int
+);
+insert into car_travels values ('Car1', 'Day1', 50);
+insert into car_travels values ('Car1', 'Day2', 100);
+insert into car_travels values ('Car1', 'Day3', 200);
+insert into car_travels values ('Car2', 'Day1', 0);
+insert into car_travels values ('Car3', 'Day1', 0);
+insert into car_travels values ('Car3', 'Day2', 50);
+insert into car_travels values ('Car3', 'Day3', 50);
+insert into car_travels values ('Car3', 'Day4', 100);
+
+select * from car_travels;
+--- **************************************************************************************** ---               
+
+
+
+
+--- Q3 : Find actual distance --- 
+
+-- Solution:
+select *
+,lag(cumulative_distance, 1, 0) over(partition by cars order by days)
+, cumulative_distance - lag(cumulative_distance, 1, 0) over(partition by cars order by days) as distance_travelled
+from car_travels;
+
+
+--- **************************************************************************************** ---   
 
 
 
