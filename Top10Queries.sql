03f099a937193afd13cc0cd6d58a212fec5734e2
Miguel Torres <migangtor@hotmail.com>
2024-05-18 12:46:00 +0100

    feat: add query 6

--- Top10Queries.sql
+++ Top10Queries.sql
@@ -173,9 +173,70 @@ insert into travel_items values (1, 'Water Bottle', 2);
 insert into travel_items values (2, 'Tent', 1);
 insert into travel_items values (3, 'Apple', 4);
 
+select *
+from travel_items
+
+--- **************************************************************************************** ---               
 
 
+--- Q5 : Ungroup the given input data --- 
 
+-- Solution:
+with recursive cte as
+    (select id, item_name, total_count, 1 as level
+    from travel_items
+    union all
+    select cte.id, cte.item_name, cte.total_count - 1, level+1 as level
+    from cte
+    join travel_items t on t.item_name = cte.item_name and t.id = cte.id
+    where cte.total_count > 1
+    )
+select id, item_name
+from cte
+order by 1;
+
+
+--- **************************************************************************************** ---               
+
+--- Q6 : IPL Matches --- 
+
+drop table if exists teams;
+create table teams
+    (
+        team_code       varchar(10),
+        team_name       varchar(40)
+    );
+
+insert into teams values ('RCB', 'Royal Challengers Bangalore');
+insert into teams values ('MI', 'Mumbai Indians');
+insert into teams values ('CSK', 'Chennai Super Kings');
+insert into teams values ('DC', 'Delhi Capitals');
+insert into teams values ('RR', 'Rajasthan Royals');
+insert into teams values ('SRH', 'Sunrisers Hyderbad');
+insert into teams values ('PBKS', 'Punjab Kings');
+insert into teams values ('KKR', 'Kolkata Knight Riders');
+insert into teams values ('GT', 'Gujarat Titans');
+insert into teams values ('LSG', 'Lucknow Super Giants');
+commit;
+
+-- 1) Write an sql query such that each team play with every other team just once. 
+
+select *
+from teams;
+
+--- Q6 : IPL Matches --- 
+
+-- Solution for 1: Each team plays with every other team JUST ONCE.
+WITH matches AS
+    (SELECT row_number() over(order by team_name) AS id, t.*
+     FROM teams t)
+SELECT team.team_name AS team, opponent.team_name AS opponent
+FROM matches team
+JOIN matches opponent ON team.id < opponent.id
+ORDER BY team;
+
+
+--- **************************************************************************************** ---               
 
 
 --- Q10: Pizza Delivery Status --- 
